e4428f80e88ec0e67a1c12f8107978059a560486
Witkowski Antoine <witkowski.antoine@gmail.com>
2022-01-07 13:36:58 +0100

    :art: add logo on sideBar && :art: and add some styles on site skeleton

--- Components/SideBar.jsx
+++ Components/SideBar.jsx
@@ -1,29 +1,36 @@
 import Link from "next/link";
+import { useRouter } from "next/router"
 
-const SideBar = () => (
-    <nav className="bg-blue-800 text-blue-100 w-64">
+const SideBar = () => {
+
+    const router = useRouter();
+    const active = "bg-white text-slate-400 rounded-l-lg";
+
+ return (
+    <nav className="text-white">
+        <img src="/favicon.png" alt="logo"/>
         <ul>
-            <li>
+            <li className={router.pathname == "/" ? active : ""}>
                 <Link href="/">
                     <a>ACCUEIL</a>
                 </Link>
             </li>
-            <li>
+            <li className={router.pathname == "/my-body" ? active : ""}>
                 <Link href="/my-body">
                     <a>MON CORPS</a>
                 </Link>
             </li>
-            <li>
+            <li className={router.pathname == "/shop" ? active : ""}>
                 <Link href="/shop">
                     <a>BOUTIQUE</a>
                 </Link>
             </li>
-            <li>
+            <li className={router.pathname == "/contact" ? active : ""}>
                 <Link href="/contact">
                     <a>CONTACT</a>
                 </Link>
             </li>
-            <li>
+            <li className={router.pathname == "/about" ? active : ""}>
                 <Link href="/about">
                     <a>A PROPOS</a>
                 </Link>
@@ -31,5 +38,6 @@ const SideBar = () => (
         </ul>
     </nav>
 );
+}
 
 export default SideBar;
