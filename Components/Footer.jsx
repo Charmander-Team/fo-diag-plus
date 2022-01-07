86951d658cc44b5ded8553977ae214592cbdc13e
Witkowski Antoine <witkowski.antoine@gmail.com>
2022-01-07 15:22:02 +0100

    :art: add assets && update footer

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,10 +1,29 @@
+import Link from 'next/link'
+
 const Footer = () => {
 
- return (
-    <>
-    Footer
-    </>
-);
+    return (
+        <>
+            <div className="bg-blue-300 flex">
+                <Link href="https://www.facebook.com/">
+                    <a target="_blank">
+                        <img src="/facebook-logo.png" alt="Facebook logo" width="60"/>
+                    </a>
+                </Link>
+                <Link href="https://twitter.com/?lang=fr">
+                    <a target="_blank">
+                        <img src="/twitter-logo.png" alt="Twitter logo" width="60"/>
+                    </a>
+                </Link>
+                <Link href="https://www.instagram.com/">
+                    <a target="_blank">
+                        <img src="/instagram-logo.png" alt="Instagram logo" width="60"/>
+                    </a>
+                </Link>
+            </div>
+            Footer
+        </>
+    );
 }
 
 export default Footer;
