914482c9e0b0f32ed233484d1066a45a6fab666e
Witkowski Antoine <witkowski.antoine@gmail.com>
2022-02-07 18:48:44 +0100

    :test_tube: test

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,7 +14,7 @@ const Home = () => {
             </div>
 
             <div className="pt-8 pb-8">
-                5 blocs
+                5 blocs.
             </div>
 
             <div className="pt-8 pb-8">
