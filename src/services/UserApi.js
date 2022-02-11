d17f48d88491427fb6b0f7c99568fdb43365ae0e
Witkowski Antoine <witkowski.antoine@gmail.com>
2022-02-11 17:24:44 +0100

    :lock: update api route (https)

--- src/services/UserApi.js
+++ src/services/UserApi.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const UserApi = {
     async AllUser(){
         try {
-            let response = await axios.get('http://api.diag-plus.tk/api/users/')
+            let response = await axios.get('https://api.diag-plus.tk/api/users/')
             .then((data)=>{
               return data;
             })
