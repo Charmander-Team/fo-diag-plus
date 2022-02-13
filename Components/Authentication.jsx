a3d24904fecc86d420ba077f132cbd7c34a1a3b0
Witkowski Antoine <witkowski.antoine@gmail.com>
2022-02-13 17:56:21 +0100

    step 1 authentication: Manage register/connection Component (with states/provider)

--- Components/Authentication.jsx
+++ Components/Authentication.jsx
@@ -1,11 +1,11 @@
 import usersApi from "../src/services/routes/usersApi";
-import {useEffect, useState} from 'react'
+import {useContext, useEffect, useState} from 'react'
 import Button from "./Button";
+import AuthenticationContext from "../Contexts/Authentication";
 
 const Authentication = () => {
 
     const [user, setUser] = useState([]);
-
     useEffect(() => {
         const getUsers = async () => {
             const data = await usersApi.getUsers();
@@ -14,13 +14,17 @@ const Authentication = () => {
         getUsers();
     }, [])
 
+    const { connectionClick, registerClick } = useContext(AuthenticationContext);
+
     return (
         <>
             <div className="">
                 <Button label="Connexion"
-                        classList="border-2 border-slate-400 bg-white hover:bg-slate-400 hover:text-white"/>
+                        classList="border-2 border-slate-400 bg-white hover:bg-slate-400 hover:text-white"
+                        onClick={ connectionClick }/>
                 <Button label="Inscription"
-                        classList="border-2 border-slate-400 bg-white hover:bg-slate-400 hover:text-white"/>
+                        classList="border-2 border-slate-400 bg-white hover:bg-slate-400 hover:text-white"
+                        onClick={ registerClick }/>
             </div>
             {user.map(element => element.email)}
         </>
--- Components/Register.jsx
+++ Components/Register.jsx
@@ -1,8 +1,19 @@
+import Button from "./Button";
+import AuthenticationContext from "../Contexts/Authentication";
+import {useContext} from "react";
+
 const Register = () => {
+
+    const { resetState } = useContext(AuthenticationContext);
 
     return (
         <>
             Inscription Component
+
+            <Button label="X Fermer"
+                    classList="border-2 border-slate-400 bg-red-100 hover:bg-slate-400 hover:text-white"
+                    onClick={resetState}/>
+
         </>
     );
 }
