a0bf87a9ccd0b05232f611bc206fb0b3e01d212b
Witkowski Antoine <witkowski.antoine@gmail.com>
2022-02-14 19:44:50 +0100

    :test_tube: update register components

--- Components/Register.jsx
+++ Components/Register.jsx
@@ -4,15 +4,55 @@ import {useContext} from "react";
 
 const Register = () => {
 
-    const { resetState } = useContext(AuthenticationContext);
+    const {resetState} = useContext(AuthenticationContext);
+
+    // const userRole = document.querySelector('input[name="status"]');
+    // userRole.addEventListener("click", () => {
+    //     console.log(userRole.value);
+    // })
+
+    const action = (event) => {
+        event.preventDefault();
+        console.log("inscription...")
+    }
 
     return (
         <>
-            Inscription Component
 
             <Button label="X Fermer"
                     classList="border-2 border-slate-400 bg-red-100 hover:bg-slate-400 hover:text-white"
                     onClick={resetState}/>
+
+            <form>
+
+                <input className="border-2 border-slate-400 rounded-lg" name="lastname" type="text"
+                       placeholder="DOE" required/>
+
+                <input className="border-2 border-slate-400 rounded-lg" name="firstname" type="text"
+                       placeholder="John" required/>
+
+                <input className="border-2 border-slate-400 rounded-lg" name="phone" type="tel"
+                       placeholder="0123456789" required/>
+
+                <input className="border-2 border-slate-400 rounded-lg" name="address" type="text"
+                       placeholder="1, rue du diagnostique" required/>
+
+                <input className="border-2 border-slate-400 rounded-lg" name="city" type="text"
+                       placeholder="Paris" required/>
+
+                <input className="border-2 border-slate-400 rounded-lg" name="zip" type="number"
+                       placeholder="75000" required/>
+
+                <div>
+                    <input type="radio" name="status" value="Patient"/> Patient
+                    <input type="radio" name="status" value="Doctor"/> Médecin
+                </div>
+
+                <Button label="S'inscrire"
+                        classList="border-2"
+                        onClick={action}/>
+
+            </form>
 
         </>
     );
