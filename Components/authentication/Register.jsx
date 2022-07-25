e38a815530c74583b55fb4aaf4140b364f983fb1
Witkowski Antoine <witkowski.antoine@gmail.com>
2022-07-25 15:04:34 +0200

    refactor register FORM

--- Components/authentication/Register.jsx
+++ Components/authentication/Register.jsx
@@ -6,47 +6,148 @@ import RegisterDoctor from "./RegisterDoctor";
 
 const Register = () => {
 
-    /** To close Authentication Component **/
-    const {resetState} = useContext(AuthenticationContext);
+  /** To close Authentication Component **/
+  const {resetState} = useContext(AuthenticationContext);
 
-    /** To manage radio role input **/
-    const [role, setRole] = useState(null);
-    const handleClick = (r) => {
-        setRole(r.target.value);
-    }
+  /** To manage radio role input **/
+  const [role, setRole] = useState(null);
+  const handleClick = (r) => {
+    setRole(r.target.value);
+  }
 
-    return (
-        <>
+  const addUser = (event) => {
+    event.preventDefault();
+    console.log("inscription...")
+  }
 
-            <Button label="X - Fermer"
-            classList="border-2 border-slate-400 bg-red-100 hover:bg-slate-400 hover:text-white mb-6"                    
-            onClick={resetState}/>
+  return (
+    <>
 
-            <form>
+      <Button label="X - Fermer"
+              classList="border-2 border-slate-400 bg-white hover:bg-slate-400 hover:text-white"
+              onClick={resetState}/>
 
-            <section className="register mb-4">
-                <div>
-                    <input type="radio" id="control_01" name="status" onClick={handleClick} value="Patient"/>
-                    <label htmlFor="control_01">
-                        <h2 className="mt-3">Patient</h2>
-                        <p>Je suis un patient est je souhaite obtenir un diagnostique fiable en ligne</p>
-                    </label>
-                </div>
-                <div>
-                    <input type="radio" id="control_02" name="status" onClick={handleClick} value="Doctor"/>
-                    <label htmlFor="control_02">
-                        <h2 className="mt-3">Docteur</h2>
-                        <p>Je suis un praticien et je souhaite effectuer des diagnostiques en ligne puis consulter mes patients</p>
-                    </label>
-                </div>
-            </section>
+      <form>
+        <div className="w-full pl-24 ">
+          <div className="bg-slate-300 shadow-md rounded px-8 pt-6 pb-8 mb-4">
 
-                { role === "Patient" ? <RegisterPatient/> : ""}
-                { role === "Doctor" ? <RegisterDoctor/> : ""}
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastname">
+                Nom
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="lastname" type="text" placeholder="COUALAN"/>
+            </div>
 
-            </form>
-        </>
-    );
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstname">
+                Prénom
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="firstname" type="text" placeholder="Yoann"/>
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="mail">
+                Adresse mail
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="mail" type="email" placeholder="coualan.yoann@gmail.com"/>
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password" type="password"/>
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">
+                Téléphone
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="phone" type="phone" placeholder="0601020304"/>
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="address">
+                Adresse postale
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="address" type="text" placeholder="25 Rue Claude Tillier"/>
+            </div>
+
+            <div className="mb-4 flex">
+              <div className="w-full mr-2">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="city">
+                  Ville
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="city" type="text" placeholder="Paris"/>
+              </div>
+
+              <div className="w-full">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="zip">
+                  Code postal
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="zip" type="number" placeholder="75012"/>
+              </div>
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Sexe
+              </label>
+              <div>
+                <span className="px-3">M :</span><input name="gender" value="M" type="radio"/>
+                <span className="px-3">F :</span><input name="gender" value="F" type="radio"/>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <section className="register mb-4">
+          <div>
+            <input type="radio" id="control_01" name="status" onClick={handleClick} value="Patient"/>
+            <label htmlFor="control_01">
+              <h2 className="mt-3">Patient</h2>
+              <p>Je suis un patient est je souhaite obtenir un diagnostique fiable en ligne</p>
+            </label>
+          </div>
+          <div>
+            <input type="radio" id="control_02" name="status" onClick={handleClick} value="Doctor"/>
+            <label htmlFor="control_02">
+              <h2 className="mt-3">Docteur</h2>
+              <p>Je suis un praticien et je souhaite effectuer des diagnostiques en ligne puis consulter mes
+                patients</p>
+            </label>
+          </div>
+        </section>
+
+        {role === "Patient" ? <RegisterPatient/> : ""}
+        {role === "Doctor" ? <RegisterDoctor/> : ""}
+
+        <div className="flex items-center justify-between">
+          <Button label="S'inscrire"
+                  onClick={addUser}
+                  classList="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"/>
+        </div>
+
+      </form>
+    </>
+  );
 }
 
 export default Register;
